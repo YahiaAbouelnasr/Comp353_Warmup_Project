f27f7cbd6baa61b2dba86796619cf02ca78c5460
YahiaAbouelnasr <yhelmy0401@gmail.com>
2026-07-25 15:18:20 -0400

    Add SQL queries to count records in various tables

--- sql/6_count_statements.sql
+++ sql/6_count_statements.sql
@@ -1,1 +1,61 @@
+USE wfc353_1;
 
+SELECT COUNT(*) AS LocationsCount
+FROM Locations;
+
+SELECT COUNT(*) AS LocationPhonesCount
+FROM LocationPhones;
+
+SELECT COUNT(*) AS PersonnelCount
+FROM Personnel;
+
+SELECT COUNT(*) AS FamilyMembersCount
+FROM FamilyMembers;
+
+SELECT COUNT(*) AS ClubMembersCount
+FROM ClubMembers;
+
+SELECT COUNT(*) AS HobbiesCount
+FROM Hobbies;
+
+SELECT COUNT(*) AS TeamCount
+FROM Teams;
+
+SELECT COUNT(*) AS FIFAGamesCount
+FROM FIFA_Games;
+
+SELECT COUNT(*) AS MinorsCount
+FROM Minors;
+
+SELECT COUNT(*) AS MajorsCount
+FROM Majors;
+
+SELECT COUNT(*) AS PaymentsCount
+FROM Payments;
+
+SELECT COUNT(*) AS WorksAtCount
+FROM WorksAt;
+
+SELECT COUNT(*) AS ManagesCount
+FROM Manages;
+
+SELECT COUNT(*) AS RegistersAtCount
+FROM RegistersAt;
+
+SELECT COUNT(*) AS MemberAtCount
+FROM MemberAt;
+
+SELECT COUNT(*) AS FamilyOfCount
+FROM FamilyOf;
+
+SELECT COUNT(*) AS HasHobbyCount
+FROM HasHobby;
+
+SELECT COUNT(*) AS BelongsToCount
+FROM BelongsTo;
+
+SELECT COUNT(*) AS PlaysInCount
+FROM PlaysIn;
+
+SELECT COUNT(*) AS ParticipatesInCount
+FROM ParticipatesIn;
